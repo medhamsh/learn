090ad0d0283a219ac12d290aeecb488e06376232
Medhamsh V <medhamsh@gmail.com>
2014-06-25 08:14:32 +0000

    Create a container, start and display its ip

--- container-init/create_container.rb
+++ container-init/create_container.rb
@@ -14,25 +14,12 @@ $memory = data.first.memory
 $cpus = data.first.cpus
 
 class Drops
-  c = LXC::Container.new("#{type}")
-  puts "#{$name}"
+  c = LXC::Container.new("#{$type}")
   clone = c.clone("#{$name}")
-  
-  @ip = c.ip_addresses
-  puts @ip
+  sleep(10)
+  d = LXC::Container.new("#{$name}")
+  d.start  
+  sleep(5)
+  @ip = d.ip_addresses
+  puts "#{@ip}"
 end
-
-#c = LXC::Container.new('lamp')
-#puts $name
-#clone = c.clone('$name')
-
-#d = LXC::Container.new('$name')
-#d.start
-#Running custom commands in the continer
-
-#d.attach do
-#  LXC.run_command('sudo superadmin-init')
-#end
-
-#$IP = d.ip_addresses
-
